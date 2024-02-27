3bdc08e730ae3f3c08857cb804cfa2444364b5aa
Fao2212 <49621525+Fao2212@users.noreply.github.com>
2024-02-27 02:17:43 -0600

    Power BI visual

---
+++ src/PowerBIPythonVisuals/heatmap.py
@@ -0,0 +1,13 @@
+# The following code to create a dataframe and remove duplicated rows is always executed and acts as a preamble for your script: 
+
+# dataset = pandas.DataFrame(QUANTITY_OF_ITEMS_SOLD, UNIT_PRICE)
+# dataset = dataset.drop_duplicates()
+
+# Paste or type your script code here:
+
+#import seaborn as sns
+
+#import matplotlib.pyplot as plt
+
+#hm = sns.heatmap(dataset.corr(),annot= True)
+#plt.show()
